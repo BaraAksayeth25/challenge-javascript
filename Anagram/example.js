6e747019b4ae32f927f9927b83b34003915e67d3
BaraAksayeth25 <mbara4321@gmail.com>
2021-06-14 13:48:23 +0700

    initial commit

---
+++ Anagram/example.js
@@ -0,0 +1,7 @@
+/*
+ * function anagram(bara, ['bara', 'arab', 'cafo', 'aabr'])
+ * this will return ['bara', 'arab', 'aabr']
+ *
+ * For more information about anagram you see  https://www.idntimes.com/hype/fun-fact/setia-galuh-santoso/10-anagram-paling-terkenal-di-dunia-c1c2
+ *
+ */
